3466450f09c493b45a6a8105d18f03ede8739c37
Heider Pinholi Lopes <heider.lopes@BRSPOBITANM0089.local>
2019-04-13 11:46:58 -0300

    Inclusao do Stetho

--- app/src/main/java/br/com/heiderlopes/blocodenotas/api/RetrofitAPI.kt
+++ app/src/main/java/br/com/heiderlopes/blocodenotas/api/RetrofitAPI.kt
@@ -1,7 +1,11 @@
 package br.com.heiderlopes.blocodenotas.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.facebook.stetho.okhttp3.StethoInterceptor
+
+
 
 class RetrofitAPI<T> {
 
@@ -9,10 +13,17 @@ class RetrofitAPI<T> {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://blocodenotasmeunome.herokuapp.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(getOkhttpClient())
             .build()
 
         return retrofit.create(c)
     }
+}
+
+fun getOkhttpClient(): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addNetworkInterceptor(StethoInterceptor())
+        .build()
 }
 
 fun getNotaAPI(): NotaAPI {
