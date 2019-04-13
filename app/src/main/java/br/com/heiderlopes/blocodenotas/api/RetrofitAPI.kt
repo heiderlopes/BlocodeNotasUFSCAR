71903cc39d70de13e69d49ff8e7003abc3289d5b
Heider Pinholi Lopes <heider.lopes@BRSPOBITANM0089.local>
2019-04-13 11:02:22 -0300

    Adicao de permissao de internet e gsonconverter

--- app/src/main/java/br/com/heiderlopes/blocodenotas/api/RetrofitAPI.kt
+++ app/src/main/java/br/com/heiderlopes/blocodenotas/api/RetrofitAPI.kt
@@ -1,12 +1,14 @@
 package br.com.heiderlopes.blocodenotas.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitAPI<T> {
 
     fun getClient(c: Class<T>) : T {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://blocodenotasmeunome.herokuapp.com/")
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         return retrofit.create(c)
